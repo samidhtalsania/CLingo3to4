e175a9b82fc0af6112c9a740d2412166b3a5f59e
Samidh Talsania <samidhtalsania@gmail.com>
2015-11-05 15:25:06 -0700

    finished method for translating normal rule matching

--- f2lp_wrapper.cpp
+++ f2lp_wrapper.cpp
@@ -8,6 +8,8 @@
 6 repeat till all lines are taken care of.
 7 at max should require one parse throught he file. 
 
+
+compiling command : g++ -I /usr/include/boost f2lp_wrapper.cpp -o regex_test -L /usr/lib/x86_64-linux-gnu/ -lboost_regex
 */
 
 #include <fstream>
@@ -19,16 +21,53 @@
 
 #include <boost/regex.hpp>
 #include <boost/algorithm/string.hpp>
-
 #include <boost/iostreams/filtering_stream.hpp>
 
 
+namespace io = boost::iostreams; 
+
 std::string match_ground_term_rule(const std::string input);
-std::string match_normal_rule(const std::string input);
-std::string match_counting_literal_rule(const std::string input);
+
+int match_normal_rule(std::string& output, const std::string& input)
+{
+	boost::regex expr("([ a-zA-Z0-9\\(\\)]+)(:-){1}([ ,.a-zA-Z0-9\\(\\)]+)"); 
+	if(boost::regex_match(input,expr))
+	{
+		boost::regex comma_expr("(,)");
+		std::string semi_colon_expr(";");
+		std::cout<<boost::regex_replace(output,comma_expr,semi_colon_expr);
+		return 0;
+	}
+	else
+		return 1;
+}
+
+int match_counting_literal_rule(std::String& output, const std::string& input)
+{
+	
+}
 std::string match_conditional_literal_rule(const std::string input);
 std::string match_domain_rule(const std::string input);
 std::string match_hide_rule(const std::string input);
+int match_rule(std::string& output, const std::string& input)
+{
+
+	output = "H";
+	// if (match_ground_term_rule)
+	// 	return 1
+	if (match_normal_rule(output,input))
+		return 1;
+	// if (match_counting_literal_rule)
+	// 	return 1
+	// if (match_conditional_literal_rule)
+	// 	return 1
+	// if (match_domain_rule)
+	// 	return 1
+	// if (match_hide_rule)
+	// 	return 1
+	
+	return 1;
+}
 
 struct domain_line
 {
@@ -50,39 +89,52 @@ std::string get_file_contents(const char *filename)
 
 int main(int argc, char const *argv[])
 {
-	// std::string file_content = get_file_contents("input.txt");
+
+	const char *input_file_name = "input.txt";
+	const char *output_file_name = "output.txt";
 	
-	// std::vector<std::string> nSeparatedStrings;
-	
+	std::ifstream file(input_file_name, std::ios_base::in | std::ios_base::binary);
+	std::ofstream outfile(output_file_name);
 
-	// //boost::trim_if(file_content, boost::is_any_of("\n"));
-	// boost::split(nSeparatedStrings,file_content,boost::is_any_of("\n"),boost::token_compress_on);
+	if(!outfile.is_open())
+	{
+		std::cerr << "Couldn't open output.txt \n";
+		return -1;
+	}
 
-	// //operate on each line
-	// int count= 0 ;
-
-	// for (size_t i = 0; i < nSeparatedStrings.size(); i++)
- //    {
- //    	std::vector<std::string> dotSeparatedStrings;
- //    	boost::split(dotSeparatedStrings,nSeparatedStrings[i],boost::is_any_of("."));
-    	
- //    	for (size_t j = 0; j < dotSeparatedStrings.size(); j++)
- //    	{
-	// 		//use dotSeparatedStrings[j] for the rest of the program to make changes
- //    	}	
- //    }
-
-	std::ifstream file("input.txt", std::ios_base::in | std::ios_base::binary);
     if(file)
     {
 	    try {
 
-	        boost::iostreams::filtering_istream in;
+	        io::filtering_istream in;
 	        
 	        in.push(file);
 	        for(std::string str; std::getline(in, str); )
 	        {
-	            std::cout << "Processed line " << str << '\n';
+	            //str has all the lines
+	            if(!str.empty())
+	            {
+	            	//process str. Str can be a single line ex q(1,2).q(1,3). need to separate these commands process them individually
+	            	std::vector<std::string> ind_commands;
+	            	
+            		boost::split(ind_commands,str,boost::is_any_of("."));
+
+            		for (int i = 0; i < ind_commands.size(); ++i)
+            		{
+            			/* code */
+            			boost::trim_if(ind_commands[i], boost::is_any_of("\t \n")); 
+            			std::string output;
+            			std::string input_temp(ind_commands[i]); 
+            			if(match_rule(output,input_temp))
+            			{
+            				
+            			}
+            		}
+
+	            	//output str to file
+	            	// outfile << str << std::endl;
+	            }
+
 	        }
 	    }
 	    catch(...) {
