f45809ac8e99af2123d0e6e6aea12c28827a5ec5
Samidh Talsania <samidhtalsania@gmail.com>
2015-11-16 12:40:45 -0700

    Bug fixes. Ready for testing.

--- f2lp_wrapper.cpp
+++ f2lp_wrapper.cpp
@@ -10,6 +10,9 @@
 
 
 compiling command : g++ -I /usr/include/boost f2lp_wrapper.cpp -o regex_test -L /usr/lib/x86_64-linux-gnu/ -lboost_regex
+
+Go YOLO on monday.
+
 */
 
 #include <fstream>
@@ -18,18 +21,25 @@ compiling command : g++ -I /usr/include/boost f2lp_wrapper.cpp -o regex_test -L 
 #include <cerrno>
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 #include <boost/regex.hpp>
 #include <boost/algorithm/string.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
+#include <boost/algorithm/string/replace.hpp>
 
 
-#define DEBUG 0
+// #define DEBUG 0
 #define COMMENT "%"
 #define PAREN_OPEN "("
 #define PAREN_CLOSE ")"
 #define COMMA ","
 #define COLON_DASH ":-" 
+#define DOT "."
+#define DOTCHAR '.'
+#define WHITESPACE "\t \n"
+#define HASHTAG "#"
+#define NEWLINE "\n"
 
 
 namespace io = boost::iostreams; 
@@ -39,10 +49,7 @@ std::vector< std::pair<std::string,std::string> > domain_list;
 
 std::set<std::string> search_domain_variables(const std::string& input)
 {
-
-	
 	std::set<std::string> domains;
-
 	
 	// boost::regex expr("([A-Z]+)");
 	boost::regex expr("([A-Z]+)([A-Za-z0-9]*)");
@@ -52,7 +59,9 @@ std::set<std::string> search_domain_variables(const std::string& input)
 
     for (; iter != end; ++iter)
     {
+    	#ifdef DEBUG
     	std::cout<<*iter<<std::endl;
+    	#endif
 		domains.insert(std::string(*iter));
    	}
 	
@@ -97,20 +106,22 @@ std::pair<std::string,std::string> get_domain_variables(const std::string& input
 /*
 Matches all clauses that statrt with #domain. 
 */
-int match_domain_rule(const std::string& input)
+int match_domain_rule(std::string& output, const std::string& input)
 {
 	//first check if the clause is a domain. If it is a domain clause, extract the identifier and variable from it and exit
 	if(isDomain(input))
 	{
-		// The sentence is a domain. Extract Domin identfier and domain variable and put it in a list
+		// The sentence is a domain. Extract Domain identfier and domain variable and put it in a list
+
 		domain_list.push_back(get_domain_variables(input));
+		// Comment out the domain as it is no longer used
+		output.insert(0,COMMENT);
 		return 1;
 	}
 	else
 	{
 		return 0;
 	}
-	//second check if the sentence has any variable in it that might be a domain variable. If it has then do appropriate operations to remove those.
 }
 
 /*
@@ -120,72 +131,84 @@ All senstences need to be checked for domain variables
 */
 void remove_domain_variables(std::string& output,const std::string& input)
 {
-	std::set<std::string> domains = search_domain_variables(input);
-	output = input ;
-	if(domains.size() > 0)
+
+	//if the line is a comment or begins with # ignore
+	if(input.compare(0,1,HASHTAG) == 0 || input.compare(0,1,COMMENT) == 0)
 	{
-		/* Found a domain in this input.
+		
+		return;
+	}
+	else
+	{
+		std::set<std::string> domains = search_domain_variables(input);
+		
+		
 
-		strategy 1:
-		input types:
-		1-> {loc(X,Y)}. 
-
-			Now although X and Y can both be a global 
-			variables, there is a global variable associated 
-			with X but not with Y. So we only process for X
-			and assume that Y is a local variable.				
-
-		*/
-
-		/*For each global variable detected from a string and 
-		append it to the end of the current clause. Match the 
-		identifier of the global variable from the domain_list vector.*/
-
-		/*This is the generated strnig that needs to be appended to 
-		original string */
-		std::string generated_string;
-		std::set<std::string>::iterator i;
-		for (i = domains.begin(); i != domains.end(); ++i)
+		output = input ;
+		if(!domains.empty())
 		{
-			
-			for (int j = 0; j < domain_list.size(); ++j)
-			{
+			/* Found a domain in this input.
 
-				
-				if ((*i).compare(domain_list.at(j).first) == 0)
+			strategy 1:
+			input types:
+			1-> {loc(X,Y)}. 
+
+				Now although X and Y can both be a global 
+				variables, there is a global variable associated 
+				with X but not with Y. So we only process for X
+				and assume that Y is a local variable.				
+
+			*/
+
+			/*For each global variable detected from a string and 
+			append it to the end of the current clause. Match the 
+			identifier of the global variable from the domain_list vector.*/
+
+			/*This is the generated string that needs to be appended to 
+			original string */
+			std::string generated_string;
+			std::set<std::string>::iterator i;
+			for (i = domains.begin(); i != domains.end(); ++i)
+			{
+				for (int j = 0; j < domain_list.size(); ++j)
 				{
-					
-					generated_string.append(domain_list.at(j).second)
-					.append(PAREN_OPEN)
-					.append(domain_list.at(j).first)
-					.append(PAREN_CLOSE)
-					.append(COMMA);
-					
+					if ((*i).compare(domain_list.at(j).first) == 0)
+					{
+						generated_string.append(domain_list.at(j).second)
+						.append(PAREN_OPEN)
+						.append(domain_list.at(j).first)
+						.append(PAREN_CLOSE)
+						.append(COMMA);
+					}
 				}
 			}
-		}
 
-		/* The last clause will have a trailing comma, remove that*/
-		ba::trim_right_if(generated_string,ba::is_any_of(COMMA));
+			/* The last clause will have a trailing comma, remove that*/
+			ba::trim_right_if(generated_string,ba::is_any_of(COMMA));
 
-		//detect if a string has ':-' in it
-		//If it doesn't then append a ':-' to the clause and
-		//then append the generated string
-		if(input.find(":-") == std::string::npos)
-		{
-			output.append(COLON_DASH)
-			.append(generated_string);
-			#ifdef DEBUG
-			std::cout<<"final string:"<<output<<std::endl;
-			#endif
+			//detect if a string has ':-' in it
+			//If it doesn't then append a ':-' to the clause and
+			//then append the generated string
+			if(input.find(":-") == std::string::npos)
+			{
+				output.append(COLON_DASH)
+				.append(generated_string);
+				#ifdef DEBUG
+				std::cout<<"final string:"<<output<<std::endl;
+				#endif
+			}
+			else
+			{
+				output.append(COMMA)
+				.append(generated_string);
+				#ifdef DEBUG
+				std::cout<<"final string:"<<output<<std::endl;
+				#endif
+			}
 		}
 		else
 		{
-			output.append(COMMA)
-			.append(generated_string);
-			#ifdef DEBUG
-			std::cout<<"final string:"<<output<<std::endl;
-			#endif
+			return;
 		}
 	}
 }
@@ -197,7 +220,10 @@ int match_normal_rule(std::string& output, const std::string& input)
 	{
 		boost::regex comma_expr("(,)");
 		std::string semi_colon_expr(";");
+		boost::regex_replace(output,comma_expr,semi_colon_expr);
+		#ifdef DEBUG
 		std::cout<<boost::regex_replace(output,comma_expr,semi_colon_expr);
+		#endif
 		return 1;
 	}
 	else
@@ -208,7 +234,10 @@ int match_normal_rule(std::string& output, const std::string& input)
 int match_counting_literal_rule(std::string& output, const std::string& input)
 {
 	boost::regex expr("([0-9 ]*) ([{]){1} ([A-Za-z0-9\\(\\),:<>= ]+) ([}]){1} ([0-9 ]*)"); 
+	
+	#ifdef DEBUG
 	std::cout<<boost::regex_match(input,expr)<<std::endl;
+	#endif
 
 	output = input;
 
@@ -290,7 +319,7 @@ int match_counting_literal_rule(std::string& output, const std::string& input)
 int match_hide_rule(std::string& output, const std::string& input)
 {
 	boost::regex expr("(#hide)"); 
-	// std::cout<<boost::regex_match(s,expr)<<std::endl;
+	
 	
 	#ifdef DEBUG
 		//c++ true = 1 false = 0
@@ -312,10 +341,37 @@ int match_hide_rule(std::string& output, const std::string& input)
 		return 0;
 }
 
+//Every string will match one of these rules. If it wont match the string is written as it is.
+//Every string after its transformation also needs to be checked for domain variables.
+//If it contains domain variables then in that case those variables need to be removed.
 int match_rule(std::string& output, const std::string& input)
 {
-
-	return 1;
+	if (match_hide_rule(output,input))
+	{
+		// std::cout<<"hide rule matched"<<std::endl;
+		return 1;
+	}
+	else if (match_counting_literal_rule(output,input))
+	{
+		// std::cout<<"counting rule matched"<<std::endl;
+		return 1;
+	}
+	else if (match_normal_rule(output,input))
+	{
+		// std::cout<<"normal rule matched"<<std::endl;
+		return 1;
+	}
+	else if (match_domain_rule(output,input))
+	{
+		// std::cout<<"domain rule matched"<<std::endl;
+		return 1;
+	}
+	else
+	{
+		// std::cout<<"No rule matched"<<std::endl;
+		return 0;
+	}
+	
 }
 
 
@@ -361,22 +417,46 @@ int main(int argc, char const *argv[])
 	            	//process str. Str can be a single line ex q(1,2).q(1,3). need to separate these commands process them individually
 	            	std::vector<std::string> ind_commands;
 	            	
-            		boost::split(ind_commands,str,boost::is_any_of("."));
+	            	// First you need to check if it is a range statement.
+	            	// Example. number(1..8) should not be split at the innner dots.
+	            	// One way to do this is convert the first pair of dots to ",," do the split then reconvert it back to ".."
+	            	std::size_t found = str.find("..");
+	            	
+	            	if(found != std::string::npos)
+	            	{
+	            		boost::replace_all(str, "..", ",,");
+	            	}
+
+            		boost::split(ind_commands,str,boost::is_any_of(DOT));
 
             		for (int i = 0; i < ind_commands.size(); ++i)
             		{
             			/* code */
-            			boost::trim_if(ind_commands[i], boost::is_any_of("\t \n")); 
-            			std::string output;
-            			std::string input_temp(ind_commands[i]); 
-            			if(match_rule(output,input_temp))
+            			boost::trim_if(ind_commands[i], boost::is_any_of(WHITESPACE)); 
+            			
+            			std::string input_temp(ind_commands[i]);
+            			std::string output(input_temp); 
+						
+						match_rule(output,input_temp);
+            			remove_domain_variables(output,output);
+						output.append(DOT).append(NEWLINE);
+						
+						//Reversing the above operation
+            			if(found != std::string::npos)
             			{
-            				continue;
+            				boost::replace_all(output, ",,", "..");
             			}
-            		}
 
-	            	//output str to file
-	            	// outfile << str << std::endl;
+            			// Dirty hack to remove all outputs with just a dot. need to figure out why
+
+            			if (output.size() > 2)
+        				{
+        					outfile << output;
+        				}
+
+            			
+        				
+            		}
 	            }
 
 	        }
